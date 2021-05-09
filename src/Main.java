577eac4248debd02c0da2e47cbdb9b20f702020a
thantai01 <thantaihp2011@gmail.com>
2021-05-09 14:37:13 +0700

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +15,29 @@ public class Main {
     }
 
 
+    static void programRun(int[] array) {
+        int action;
+        do {
+            Scanner sc = new Scanner(System.in);
+            menuAction();
+            System.out.println("What next?! Choose an action below: ");
+            action = sc.nextInt();
+            switch (action) {
+                case 1:
+                    arrayDisplay(array);break;
+                case 2:
+                    System.out.println("Largest even number: " + largestEvenNums(array));break;
+                case 3:
+                    displayEven(array,evenNumber(array));break;
+                case 4:
+                    sumOfEven(array);break;
+                case 5:
+
+                case 10:
+                    System.exit(0);
+            }
+        } while (action > 0 && action < 11) ;
+    }
 
     static int sizeIn() {
         Scanner sc = new Scanner(System.in);
@@ -29,32 +53,14 @@ public class Main {
         System.out.println("Menu");
         System.out.println("1. Display your array");
         System.out.println("2. Find a largest even number in array");
-        System.out.println("3. ");
-        System.out.println("11. Exit");
-    }
-
-    static void programRun(int[] array) {
-        int action;
-        do {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("What next?! Choose an action below: ");
-            menuAction();
-            action = sc.nextInt();
-            switch (action) {
-                case 1:
-                    arrayDisplay(array);
-                    break;
-                case 2:
-
-                    System.out.println("Largest even number: " + largestEvenNums(array));
-                    break;
-                case 3:
-                    break;
-                case 4:
-                case 11:
-                    System.exit(0);
-            }
-        } while (action > 0 && action <= 11) ;
+        System.out.println("3. Display all even number in array");
+        System.out.println("4. Sum of even number");
+        System.out.println("5. Insert new item to array");
+        System.out.println("6. Delete item in array");
+        System.out.println("7. Reverse an array ");
+        System.out.println("8. Prime number in array ");
+        System.out.println("9. Filter array from max to min ");
+        System.out.println("10. Exit");
     }
 
     static int[] arrayRandomCreate(int l) {
@@ -77,6 +83,31 @@ public class Main {
                     maxEven = j;
         }
         return maxEven;
+    }
+
+    static boolean evenNumber(int[] array) {
+        for (int i = 0;i< array.length;i++) {
+                if (array[i] % 2 == 0)
+                    return true;
+            }
+        return false;
+    }
+
+    static void displayEven(int[] array, boolean evenNumber) {
+            for (int i = 0; i < array.length;i++) {
+                if(evenNumber)
+                    System.out.println(array[i] + ", ");
+            }
+        }
+
+    static void sumOfEven(int[] array) {
+        int sum = 0;
+        for(int i = 0;i<array.length;i++) {
+            if(evenNumber(array)) {
+                sum+= array[i];
+            }
+        }
+        System.out.println("Sum of even = " + sum);
     }
 
 }
